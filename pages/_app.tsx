9ace634e9ccbadea50e42aface982fbbfe74bcd0
kitety <aizaizuori@foxmail.com>
2021-09-01 00:12:48 +0800

    feat: add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log("pageProps: ", pageProps);
+
+  return (
+    <>
+      <Head>
+        <title>Test</title>
+        <link
+          href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
